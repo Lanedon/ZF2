57639eeea0fcc5a9f161379a84fcec9cbaeae15e
Matthias Galvez - Dev2 <ma.galvez@laposte.net>
2015-12-11 17:09:46 +0100

    Bootstrap

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 chdir(dirname(__DIR__));
 
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 // Run the application!
